32eb3b0c38f93aaf22a156fe41ec9e05df5aeec4
Helio Loureiro <helio.loureiro@ericsson.com>
2021-10-03 15:12:43 +0200

    To create quick workable schedule.

---
+++ scripts/dump_pretalx_csv.py
@@ -0,0 +1,94 @@
+#! /usr/bin/python3
+
+import requests, json, os, sys, re
+import csv
+
+
+URL="https://pretalx.com/pycon-sweden-2021/schedule/export/schedule.json"
+URLAUTHORS="https://pretalx.com/api/events/pycon-sweden-2021/speakers/"
+
+
+def debug(*msg):
+    if os.environ.get("DEBUG"):
+        print(*msg)
+
+def curl(url):
+    authorization_token = os.environ.get("PRETALX_TOKEN")
+    if authorization_token is None:
+        raise Exception("Missing pretalx's authorization token on environment variable PRETALX_TOKEN.")
+
+    headers = { "Authorization" : f"Token {authorization_token}"}
+
+    req = requests.get(url, headers=headers)
+
+    if req.status_code != 200:
+        raise Exception("Failed to fetch schedule:", req.status_code)
+    return req.text
+
+jSchedule = json.loads(curl(URL))
+debug(json.dumps(jSchedule, indent=4))
+jAuthors = json.loads(curl(URLAUTHORS))
+debug(json.dumps(jAuthors, indent=4))
+
+author_map = {}
+for author_data in jAuthors["results"]:
+    email = author_data["email"]
+    name = author_data["name"]
+    author_map[name] = email
+
+with open("pycon-sweden-2021.csv", "w", newline='') as csvfile:
+    fieldnames = [ "date", "start", "duration", "track", "title", "author", "email" ]
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+
+    for day in jSchedule["schedule"]["conference"]["days"]:
+        debug("Day:", day)
+        for room in day["rooms"]:
+            debug(" * room:", room)
+            for talk in day["rooms"][room]:
+                debug(" * * talk:", talk)
+                title = talk["title"]
+                abstract = talk["abstract"]
+                description = talk["description"]
+                authors = talk["persons"]
+                date = talk["date"]
+                date = re.sub("T.*", "", date)
+                start = talk["start"]
+                duration = talk["duration"]
+                track = talk["room"]
+
+                author = ""
+                if len(authors) == 1:
+                    author = authors[0]["public_name"]
+                    try:
+                        email = author_map[author]
+                    except KeyError:
+                        email = "none"
+
+                else:
+                    authors = []
+                    emails = []
+                    for identity in authors:
+                        authors.append(identity["public_name"])
+                        try:
+                            emails.append(author_map[author])
+                        except KeyError:
+                            emails.append("none")
+                    author = " and ".join(authors)
+                    email = " and ".join(emails)
+                line = f"{date},{start},{duration},{track},{title},{author},{email}"
+                debug(line)
+                writer.writerow({
+                    "date": date,
+                    "start" : start,
+                    "duration" : duration,
+                    "track" : track,
+                    "title" : title,
+                    "author" : author,
+                    "email" : email
+                })
+
+
+
+
+
